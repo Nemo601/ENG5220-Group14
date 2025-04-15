62718bca5e1eb8f4b1c047a225941b3000a61583
ShiaoHu <3030874H@student.gla.ac.uk>
2025-04-15 16:21:47 +0100

    4th motor_controller.cpp

--- motor_controller.cpp
+++ motor_controller.cpp
@@ -1,22 +1,3 @@
-#include <wiringPi.h>
-#include <iostream>
-#include <map>
-#include <vector>
-#include <string>
-#include <unistd.h>
-#include "StepperMotor.h"
-
-using namespace std;
-
-string getDetectedType() {
-    static int i = 0;
-    vector<string> types = {"recyclable", "organic", "hazardous", "other"};
-    string type = types[i % types.size()];
-    i++;
-    sleep(2); // Analogue recognition delay
-    return type;
-}
-
 int main() {
     wiringPiSetupGpio();  // Using BCM Pin Numbering
 
@@ -35,20 +16,7 @@ int main() {
     };
 
     cout << "Launch of Intelligent Waste Separation System..." << endl;
+    cout << "Press the following keys to simulate the input of a rubbish type：" << endl;
+    cout << "r - recyclable  o - organic  h - hazardous  t - other  q - quit" << endl;
 
-    while (true) {
-        string type = getDetectedType();
-        cout << "Identify to type：" << type << endl;
-
-        if (motorMap.count(type)) {
-            for (auto motor : motorMap[type]) {
-                motor->rotate(100);
-                usleep(500 * 1000);
-            }
-        } else {
-            cout << "Unknown type, skip processing." << endl;
-        }
-    }
-
-    return 0;
-}
+   
