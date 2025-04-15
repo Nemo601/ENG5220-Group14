22800c6206191bb6c5be91301f7989d0b48c7b1b
ShiaoHu <3030874H@student.gla.ac.uk>
2025-04-15 16:00:14 +0100

    2nd StepperMotor.cpp

--- StepperMotor.cpp
+++ StepperMotor.cpp
@@ -1,25 +1,32 @@
 #include "StepperMotor.h"
+#include <wiringPi.h>
+#include <unistd.h>
 
-StepperMotor::StepperMotor(const std::vector<int>& pins) : _pins(pins) {
-    for (int pin : _pins) {
+const int StepperMotor::sequence[8][4] = {
+    {1, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 1, 0},
+    {0, 0, 1, 0}, {0, 0, 1, 1}, {0, 0, 0, 1}, {1, 0, 0, 1}
+};
+
+StepperMotor::StepperMotor(const std::vector<int>& gpioPins) : pins(gpioPins) {
+    for (int pin : pins) {
         pinMode(pin, OUTPUT);
         digitalWrite(pin, LOW);
     }
 }
 
-void StepperMotor::step(int steps, bool clockwise, int speedMs) {
-    for (int i = 0; i < steps; i++) {
-        int idx = clockwise ? i % 8 : (7 - (i % 8));
-        for (int j = 0; j < 4; j++) {
-            digitalWrite(_pins[j], _sequence[idx][j]);
+void StepperMotor::rotate(int steps, bool clockwise, int delayMs) {
+    for (int i = 0; i < steps; ++i) {
+        int idx = clockwise ? i % 8 : 7 - (i % 8);
+        for (int j = 0; j < 4; ++j) {
+            digitalWrite(pins[j], sequence[idx][j]);
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(speedMs));
+        usleep(delayMs * 1000);
     }
     release();
 }
 
 void StepperMotor::release() {
-    for (int pin : _pins) {
+    for (int pin : pins) {
         digitalWrite(pin, LOW);
     }
 }
