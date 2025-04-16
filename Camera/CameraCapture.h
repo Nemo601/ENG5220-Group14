b58153776e4c40c350ff62fa95fa484e6f02df04
Xuan Liu <liuxuansheen@outlook.com>
2025-04-16 12:53:58 +0100

    Add files via upload

---
+++ Camera/CameraCapture.h
@@ -0,0 +1,19 @@
+#ifndef CAMERACAPTURE_H
+#define CAMERACAPTURE_H
+
+#include <opencv2/opencv.hpp>
+#include <string>
+
+class CameraCapture {
+public:
+    CameraCapture(int cameraIndex = 0, const std::string& path = "/home/pi/project/cpp_demo/ncnn/include/captured.jpg");
+    ~CameraCapture();
+
+    bool captureImage(); // Save directly to the specified path
+
+private:
+    cv::VideoCapture cap;
+    std::string savePath; // Storage path
+};
+
+#endif
