acdbc7d3a7f456e6bbebf2bf79dfe2c40b1ef4ee
ShiaoHu <3030874H@student.gla.ac.uk>
2025-04-19 11:54:08 +0100

    9th motor_controller.cpp

--- motor_controller.cpp
+++ motor_controller.cpp
@@ -1,91 +1,59 @@
 #include <iostream>
-#include <string>
-#include <atomic>
-#include <thread>
-#include <chrono>
 #include <wiringPi.h>
-#include "StepperMotor.h"
+#include "GarbageSorter.h"
 #include "Timer.h"
 
 using namespace std;
 
 int main() {
     wiringPiSetupGpio();
-
-    StepperMotor motor1({4, 17, 23, 24});
-    StepperMotor motor2({5, 6, 12, 13});
-    StepperMotor motor3({16, 20, 21, 26});
-    StepperMotor motor4({18, 19, 22, 27});
-
-    startTimerThread(); // Starting a timed task thread
-
-    atomic<bool> isBusy(false);
+    startTimerThread();
+    
+    // Initialise motor pinout
+    vector<vector<int>> motorPins = {
+        {4, 17, 23, 24},  // Motor 1
+        {5, 6, 12, 13},    // motor 2
+        {16, 20, 21, 26},  // motor 3
+        {18, 19, 22, 27}   // motor 4
+    };
+    
+    GarbageSorter sorter(motorPins);
+    
+    // Setting Status Callbacks
+    sorter.setStatusCallback([](const string& msg) {
+        cout << "[system status] " << msg << endl;
+    });
+    
+    sorter.startSystem();
+    
     string input;
-
-    cout << " Rubbish sorting system activated. Please enter recognition result (1~4 or q):" << endl;
-
+    cout << "The waste sorting system is activated.Please enter the identification result (1~4 or q to exit):" << endl;
+    
     while (true) {
-        // Wait for the current task to complete before allowing input
-        while (isBusy.load()) {
-            this_thread::sleep_for(chrono::milliseconds(100));
-        }
-
-        cout << "\nEnter the recognition result (1 to 4 or q): ";
+        cout << "\nEnter identification result (1-4 or q): ";
         cin >> input;
-
-        if (input == "q") break;
-
-        isBusy.store(true); // Setting Busy Status
-
-        if (input == "1") {
-            cout << " Recyclable waste disposal" << endl;
-            motor1.rotate(500, true);
-            motor2.rotate(500, true);
-
-            scheduleTask(1, [&]() {
-                motor1.rotate(500, false);
-                motor2.rotate(500, false);
-                cout << " Recyclable withdrawal completed" << endl;
-                isBusy.store(false);
-            });
+        
+        if (input == "q") {
+            break;
         }
-        else if (input == "2") {
-            cout << " Organic waste disposal" << endl;
-            motor2.rotate(1000, true);
-
-            scheduleTask(1, [&]() {
-                motor2.rotate(1000, false);
-                cout << " Organic withdrawal completed" << endl;
-                isBusy.store(false);
-            });
-        }
-        else if (input == "3") {
-            cout << " Hazardous waste disposal" << endl;
-            motor1.rotate(100, true);
-
-            scheduleTask(1, [&]() {
-                motor1.rotate(100, false);
-                cout << "Hazardous withdrawal completed" << endl;
-                isBusy.store(false);
-            });
-        }
-        else if (input == "4") {
-            cout << " Other waste disposal" << endl;
-            motor4.rotate(100, true);
-
-            scheduleTask(1, [&]() {
-                motor4.rotate(100, false);
-                cout << " Other withdrawal completed" << endl;
-                isBusy.store(false);
-            });
-        }
-        else {
-            cout << " Invalid input. Please enter 1 to 4 or q." << endl;
-            isBusy.store(false);
+        
+        try {
+            int type = stoi(input);
+            if (type >= 1 && type <= 4) {
+                sorter.processWaste(static_cast<GarbageSorter::WasteType>(type));
+                
+                // Wait for the action to complete
+                sorter.waitUntilIdle();
+            } else {
+                cout << "Invalid input, please enter 1-4 or q" << endl;
+            }
+        } catch (...) {
+            cout << "Invalid input, please enter 1-4 or q" << endl;
         }
     }
-
-    stopTimerThread(); // Stopping the timer thread
-    cout << " System exited." << endl;
+    
+    sorter.stopSystem();
+    stopTimerThread();
+    cout << "The system has exited." << endl;
     return 0;
 }
