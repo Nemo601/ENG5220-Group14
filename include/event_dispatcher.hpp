fe74417b3909f1b1fb9b01789b58bd682ee2597c
Xuan Liu <liuxuansheen@outlook.com>
2025-04-20 15:05:22 +0100

    Delete include directory

--- include/event_dispatcher.hpp
+++
@@ -1,8 +0,0 @@
-class EventDispatcher {
-public:
-    using Callback = std::function<void(const std::string&)>;
-    void registerCallback(Callback cb);
-    void dispatchEvent(const std::string& event);
-private:
-    std::vector<Callback> callbacks;
-};
