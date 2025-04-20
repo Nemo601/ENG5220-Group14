e8ba2d56f80a76b4de7654b5460339f5aaa6c041
ShiaoHu <3030874H@student.gla.ac.uk>
2025-04-20 16:10:27 +0100

    Update Timer.h

--- ENG5220-Group14/include/Timer.h
+++ ENG5220-Group14/include/Timer.h
@@ -2,6 +2,17 @@
 #define TIMER_H
 
 #include <functional>
+#include <chrono>
+#include <atomic>
+
+class IntervalTimer {
+public:
+    void setInterval(std::function<void()> func, int interval_ms);
+    void stop();
+
+private:
+    std::atomic<bool> active{true};
+};
 
 void startTimerThread();
 void stopTimerThread();
