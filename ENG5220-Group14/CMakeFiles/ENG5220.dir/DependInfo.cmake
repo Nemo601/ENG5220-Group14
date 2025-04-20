9cf879c8bf770cfb391a2e1ecc1f0c157c63d55c
Xuan Liu <liuxuansheen@outlook.com>
2025-04-20 21:06:19 +0100

    Add files via upload

---
+++ ENG5220-Group14/CMakeFiles/ENG5220.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pi/ENG5220/src/CameraCapture.cpp" "CMakeFiles/ENG5220.dir/src/CameraCapture.cpp.o" "gcc" "CMakeFiles/ENG5220.dir/src/CameraCapture.cpp.o.d"
+  "/home/pi/ENG5220/src/CameraTimer.cpp" "CMakeFiles/ENG5220.dir/src/CameraTimer.cpp.o" "gcc" "CMakeFiles/ENG5220.dir/src/CameraTimer.cpp.o.d"
+  "/home/pi/ENG5220/src/CameraWorker.cpp" "CMakeFiles/ENG5220.dir/src/CameraWorker.cpp.o" "gcc" "CMakeFiles/ENG5220.dir/src/CameraWorker.cpp.o.d"
+  "/home/pi/ENG5220/src/GarbageSorter.cpp" "CMakeFiles/ENG5220.dir/src/GarbageSorter.cpp.o" "gcc" "CMakeFiles/ENG5220.dir/src/GarbageSorter.cpp.o.d"
+  "/home/pi/ENG5220/src/StepperMotor.cpp" "CMakeFiles/ENG5220.dir/src/StepperMotor.cpp.o" "gcc" "CMakeFiles/ENG5220.dir/src/StepperMotor.cpp.o.d"
+  "/home/pi/ENG5220/src/ThreadController.cpp" "CMakeFiles/ENG5220.dir/src/ThreadController.cpp.o" "gcc" "CMakeFiles/ENG5220.dir/src/ThreadController.cpp.o.d"
+  "/home/pi/ENG5220/src/Timer.cpp" "CMakeFiles/ENG5220.dir/src/Timer.cpp.o" "gcc" "CMakeFiles/ENG5220.dir/src/Timer.cpp.o.d"
+  "/home/pi/ENG5220/src/main.cpp" "CMakeFiles/ENG5220.dir/src/main.cpp.o" "gcc" "CMakeFiles/ENG5220.dir/src/main.cpp.o.d"
+  "/home/pi/ENG5220/src/yolo_detector.cpp" "CMakeFiles/ENG5220.dir/src/yolo_detector.cpp.o" "gcc" "CMakeFiles/ENG5220.dir/src/yolo_detector.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
